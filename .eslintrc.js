180e63b2aea258c0240a78a650473ac2499c467e
Thiago Cerutti <thiago.cerutti@rdstation.com>
2024-01-03 09:10:37 -0300

    chore: clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,11 @@ module.exports = {
     ecmaVersion: 2018,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
-  plugins: ["dashboard-g11n", "example", "check-external-url"],
+  plugins: ["dashboard-g11n", "check-external-url"],
   processor: "dashboard-g11n/dataUploaderProcessor",
   rules: {
     "no-undef": 0,
     "no-unused-vars": 0,
-    "example/enforce-foo-bar": 0,
     "check-external-url/external-url-found": 1,
   },
 };
--- rules/enforceFooBar/eslint-plugin-example.js
+++
@@ -1,3 +0,0 @@
-const fooBarRule = require("./enforce-foo-bar");
-const plugin = { rules: { "enforce-foo-bar": fooBarRule } };
-module.exports = plugin;
